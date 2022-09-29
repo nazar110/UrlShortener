aa6dbd17041283910ceae3af6a6a980b7239a077
nazar110 <nazar1124@gmail.com>
2022-09-29 03:49:13 +0300

    Add domain logic for user entity

---
+++ UrlShortener/UrlShortener.DL/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.DL.Entities;
+
+namespace UrlShortener.DL.Repositories
+{
+    public interface IUserRepository
+    {
+        User Get(string login, string password);
+        void Add(User user);
+        void Update(User user);
+        void Remove(User user);
+        public void Save();
+    }
+}
---
+++ UrlShortener/UrlShortener.DL/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.DL.Entities;
+
+namespace UrlShortener.DL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UrlShortenerContext _context;
+        public UserRepository(UrlShortenerContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+        }
+
+        public User Get(string login, string password)
+        {
+            var user = _context.Users
+                .Where(usr => usr.Login == login && usr.Password == password)
+                .FirstOrDefault();
+            return user;
+        }
+
+        public void Remove(User user)
+        {
+            var existent = _context.Users
+                .Where(usr => usr.Login == user.Login && usr.Password == user.Password)
+                .FirstOrDefault();
+            
+            if (existent != null)
+            {
+                _context.Users.Remove(existent);
+            }
+        }
+
+        public void Update(User user)
+        {
+            _context.Users.Update(user);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
